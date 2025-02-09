ddc8e12037e34d45c53ff77507908835e9ccc3c8
Akarabo05 <akarabo.ines@gmail.com>
2025-02-09 15:09:35 +0000

    Added Assignment1.c

---
+++ Assignment1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, result;
+
+    // Input two numbers
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    // Subtraction
+    result = num1 - num2;
+
+    // Display result
+    printf("Result: %d - %d = %d\n", num1, num2, result);
+
+    return 0;
+}
